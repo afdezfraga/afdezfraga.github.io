0476c6dc0be29e9e183b8ffb27808962fcff8df4
Óscar Castellanos Rodríguez <oscar.castellanos@udc.es>
2021-03-04 13:46:17 +0100

    Update test_write_file.py

--- test_write_file.py
+++ test_write_file.py
@@ -4,16 +4,13 @@ import pathlib
 
 print(pathlib.Path().absolute())
 
-file = open("testfile.txt","rw") 
+file = open("testfile.txt","w") 
 
 file.write("Hello World\n") 
 file.write("This is our new text file\n") 
 file.write("and this is another line.\n") 
 file.write("Why? Because we can.\n") 
- 
-for line in file: 
-	print(line) 
- 
+  
 file.close() 
 
 print(pathlib.Path(__file__).parent.absolute())
