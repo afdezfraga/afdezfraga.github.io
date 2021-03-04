b1a913fb401f9420e7d36e0f74adad73d6d6f371
Óscar Castellanos Rodríguez <oscar.castellanos@udc.es>
2021-03-04 13:38:38 +0100

    Test

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,3 @@ ADD test_write_file.py /
 ADD test_read_file.py /
 RUN pip install pystrich
 CMD [ "python", "./test_write_file.py" ]
-ADD /testfile.txt /
--- test_write_file.py
+++ test_write_file.py
@@ -4,12 +4,16 @@ import pathlib
 
 print(pathlib.Path().absolute())
 
-file = open("testfile.txt","w") 
+file = open("testfile.txt","rw") 
 
 file.write("Hello World\n") 
 file.write("This is our new text file\n") 
 file.write("and this is another line.\n") 
 file.write("Why? Because we can.\n") 
  
+for line in file: 
+	print(line) 
+ 
 file.close() 
+
 print(pathlib.Path(__file__).parent.absolute())
