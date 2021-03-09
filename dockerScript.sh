ab154634a21265de67f409e5f462315cb03f3c66
afdezfraga <55448273+afdezfraga@users.noreply.github.com>
2021-03-09 18:19:33 +0100

    Update dockerScript.sh

--- dockerScript.sh
+++ dockerScript.sh
@@ -7,14 +7,30 @@ rm ./githubWithCommits.csv ./my_git_hub_spider/github.csv
 cd my_git_hub_spider
 scrapy crawl GitHubSpider -o github.csv -t csv
 
+echo 'CRAWLER ACABO'
+
 cd .. 
 python gitHubApiCaller.py ./my_git_hub_spider/github.csv ./githubWithCommits.csv
 
+echo 'API ACABO'
+
 git add ./githubWithCommits.csv
+
+echo 'ADD COMPLETO, SE MOSTRARA EL ESTADO'
+
+git status
+
+echo 'STATUS FINALIZADO'
 
 git config --global user.email "PE-2021q1gi@torusware.com"
 git config --global user.name "Becarios Torus"
 
+echo 'CONFIG TERMINADA'
+
 git commit -m "actualizacion periodica con fecha $(date)"
 
+echo 'COMIIT TERMINADO'
+
 git push https://$BOTNAME:$BOTPASSWD@github.com/afdezfraga/afdezfraga.github.io.git --all
+
+echo 'PUSH TERMINADO'
