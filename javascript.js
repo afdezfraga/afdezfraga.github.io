d08eba9c58461be0139fc4652a675efb74e4bcf7
Becarios Torus <PE-2021q1gi@torusware.com>
2021-03-10 11:40:54 +0000

    actualizacion periodica con fecha Wed Mar 10 11:40:54 UTC 2021

--- javascript.js
+++ javascript.js
@@ -45,8 +45,61 @@
      	
     ]);
 
+      // Create the data table.
+    var data2 = new google.visualization.DataTable();
+    data2.addColumn('string', 'Name');
+    data2.addColumn('number', 'Commits');
+    data2.addRows([
+	['alievk/avatarify-python', 351],
+     	['dapr/dapr', 1613],
+     	['jart/cosmopolitan', 236],
+     	['tauri-apps/tauri', 969],
+     	['qeeqbox/social-analyzer', 433],
+     	['android/compose-samples', 978],
+     	['CoatiSoftware/Sourcetrail', 2735],
+     	['AliaksandrSiarohin/first-order-model', 70],
+     	['ethereum-mining/ethminer', 14334],
+     	['athensresearch/athens', 546],
+     	['rxi/lite', 200],
+     	['vitejs/vite', 2250],
+     	['JonnyBurger/remotion', 1010],
+     	['MostlyAdequate/mostly-adequate-guide', 846],
+     	['aamini/introtodeeplearning', 408],
+     	['input-output-hk/cardano-node', 4000],
+     	['bitwarden/server', 2823],
+     	['iperov/DeepFaceLab', 1144],
+     	['dani-garcia/bitwarden_rs', 1425],
+     	['Immediate-Mode-UI/Nuklear', 1857],
+     	['zhangdaiscott/jeecg-boot', 459],
+     	['geekxh/hello-algorithm', 360],
+     	['freqtrade/freqtrade', 11196],
+     	['iam-abbas/Reddit-Stock-Trends', 163],
+     	['airbnb/javascript', 1861],
+     	
+    ]);
+
     // Set chart options
-    var options = {'width':'100%','height':'100%'};
+    var options = {};
+
+    var options2 = {
+        title: 'Top trending GitHub repos',
+        hAxis: {
+          title: 'Repository name',
+          format: 'h:mm a',
+          viewWindow: {
+            min: [7, 30, 0],
+            max: [17, 30, 0]
+          }
+        },
+        vAxis: {
+          title: 'Number of commits'
+        },
+        'width':'100%','height':'100%'
+      };
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart2 = new google.visualization.ColumnChart(document.getElementById('chart_div2'));
+    chart2.draw(data2, options2);
 
     // Instantiate and draw our chart, passing in some options.
     var chart = new google.visualization.Table(document.getElementById('chart_div'));
