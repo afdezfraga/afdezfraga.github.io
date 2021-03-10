b096c65acffbe0dd9ae54bf1abd02a26b8003b91
Óscar Castellanos Rodríguez <oscar.castellanos@udc.es>
2021-03-10 12:04:42 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,47 +1,57 @@
-var request = new XMLHttpRequest();
-request.open('GET', './githubWithCommits.csv', false);
-request.send();
-var textfileContent = request.responseText;
+  // Load the Visualization API and the corechart package.
+  google.charts.load('current', {'packages':['corechart']});
+  google.charts.load('current', { 'packages': ['table'] });
 
-// Load the Visualization API and the corechart package.
-google.charts.load('current', {'packages':['corechart']});
+  // Set a callback to run when the Google Visualization API is loaded.
+  google.charts.setOnLoadCallback(drawChart);
 
-// Set a callback to run when the Google Visualization API is loaded.
-google.charts.setOnLoadCallback(drawChart(textfileContent));
+  // Callback that creates and populates a data table,
+  // instantiates the pie chart, passes in the data and
+  // draws it.
+  function drawChart() {
 
-// Callback that creates and populates a data table,
-// instantiates the pie chart, passes in the data and
-// draws it.
-function drawChart(textfileContent) {
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'Top');
+    data.addColumn('string', 'Author');
+    data.addColumn('string', 'Name');
+    data.addColumn('number', 'Commits');
+    data.addRows([
+	[1, 'alievk', 'avatarify-python', 351],
+     	[2, 'dapr', 'dapr', 1613],
+     	[3, 'jart', 'cosmopolitan', 236],
+     	[4, 'tauri-apps', 'tauri', 965],
+     	[5, 'qeeqbox', 'social-analyzer', 433],
+     	[6, 'android', 'compose-samples', 978],
+     	[7, 'dolthub', 'dolt', 8580],
+     	[8, 'CoatiSoftware', 'Sourcetrail', 2735],
+     	[9, 'iam-abbas', 'Reddit-Stock-Trends', 163],
+     	[10, 'ethereum-mining', 'ethminer', 14334],
+     	[11, 'AliaksandrSiarohin', 'first-order-model', 70],
+     	[12, 'aamini', 'introtodeeplearning', 408],
+     	[13, 'athensresearch', 'athens', 540],
+     	[14, 'vitejs', 'vite', 2250],
+     	[15, 'rxi', 'lite', 200],
+     	[16, 'tgbot-collection', 'YYeTsBot', 138],
+     	[17, 'MostlyAdequate', 'mostly-adequate-guide', 846],
+     	[18, 'input-output-hk', 'cardano-node', 3996],
+     	[19, 'bitwarden', 'server', 2823],
+     	[20, 'freqtrade', 'freqtrade', 11147],
+     	[21, 'Immediate-Mode-UI', 'Nuklear', 1857],
+     	[22, 'dogecoin', 'dogecoin', 13532],
+     	[23, 'dani-garcia', 'bitwarden_rs', 1425],
+     	[24, 'iperov', 'DeepFaceLab', 1144],
+     	[25, 'airbnb', 'javascript', 1861],
+     	
+    ]);
 
-  //Configurando el archivo de entrada
-  var allTextLines = textfileContent.split(/\r\n|\n/);
-  var headers = allTextLines[0].split(',');
-  var lines = [];
+    // Set chart options
+    var options = {'title':'How Much Pizza I Ate Last Night',
+                   'width':400,
+                   'height':300};
 
-  // Create the data table.
-  var data = new google.visualization.DataTable();
-  data.addColumn('number', 'Top');
-  data.addColumn('string', 'Author');
-  data.addColumn('string', 'Name');
-  data.addColumn('number', 'Commits');
-
-  for (var i=1; i<allTextLines.length; i++) {
-    var entryData = allTextLines[i].split(',');
-    if (entryData.length == headers.length) {
-      var tarr = [];
-      for (var j=0; j<headers.length; j++) {
-        tarr.push(entryData[j]);
-      }
-      data.addRow([tarr[2], tarr[3]]);
-    
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.Table(document.getElementById('chart_div'));
+    chart.draw(data, options);
   }
-  // Set chart options
-  var options = {'title':'Ranking de commits',
-                 'width':400,
-                 'height':300};
-
-  // Instantiate and draw our chart, passing in some options.
-  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-  chart.draw(data, options);
-}
+</script>
