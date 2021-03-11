8c664d44dfeb0d3b8dfbeb49a36d2d86864e4c91
Óscar Castellanos Rodríguez <oscar.castellanos@udc.es>
2021-03-11 10:19:12 +0100

    Update con run a la maquina docker creada

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-docker build --file gittyleaks/Dockerfile -t cibuilds/docker:latest -t cibuilds/docker:19.03.5  -t cibuilds/docker:19.03 .
+docker build --file gittyleaks/Dockerfile -t gittyleaks .
+docker run gittyleaks
