29f74a19a345807db268891e3f39cce0010a165b
afdezfraga <55448273+afdezfraga@users.noreply.github.com>
2021-03-11 10:11:34 +0100

    Update build-images.sh

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build --file Dockerfile -t cibuilds/docker:latest -t cibuilds/docker:19.03.5  -t cibuilds/docker:19.03 .
+docker build --file gittyleaks/Dockerfile -t cibuilds/docker:latest -t cibuilds/docker:19.03.5  -t cibuilds/docker:19.03 .
