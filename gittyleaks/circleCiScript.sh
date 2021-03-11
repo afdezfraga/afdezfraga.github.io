4299a99f0a819cc44d9edce1882b2f7cb7ac9cd6
a.fernandez <a.fernandez3@udc.es>
2021-03-11 11:31:10 +0100

    circleci4

--- gittyleaks/circleCiScript.sh
+++ gittyleaks/circleCiScript.sh
@@ -7,6 +7,6 @@ git add gittyleaks/analisis.out
 git config --global user.email "PE-2021q1gi@torusware.com"
 git config --global user.name "Becarios Torus"
 
-git commit -m "analisis de secretos periodica con fecha $(date)"
+git commit -m "[skip ci]"
 
 git push https://$BOTNAME:$BOTPASSWD@github.com/afdezfraga/afdezfraga.github.io.git --all
